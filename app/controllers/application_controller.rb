fa8b8ceb3bb126b01a6d7436ee997e80f15a7c84
sbas23 <juan.calderon@aldeamo.com>
2018-08-24 19:46:36 -0500

    Ajustes inicio y logueo
    Ajustes inicio y logueo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+	before_action :configure_devise_params, if: :devise_controller?
+
+	def configure_devise_params
+	   devise_parameter_sanitizer.permit(:sign_up) do |user|
+	     user.permit(:name, :lastname, :document, :phone, :profession, :id_fundation, :role, :state, :email, :password, :password_confirmation)
+	   end
+	end
 end

bb5fd98fbc22fdbd917e69865af897c9d26bef0d
sbas23 <juan.calderon@aldeamo.com>
2018-08-27 08:59:17 -0500

    Se agregan recursos
    diagrama de BD

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'login', to: 'login#login' #to: define el controlador y el metodo al que apunta
   resources :index
-  resources :role
+  #resources :role
   resources :fundation
   resources :service
   resources :chat
   resources :forum
   resources :calendar
-  resources :state
+  #resources :state
 end

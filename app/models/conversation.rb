769f9a372ba06e10ffc0d45c49c256629d21d810
sbas23 <juan.calderon@aldeamo.com>
2018-08-28 17:52:51 -0500

    Solucion relacion BD
    Clase conversacion

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,10 +1,8 @@
 class Conversation < ApplicationRecord
   has_many :messages, dependent: :destroy
   
-  # Validar estas lineas generan error
-
-  #belongs_to :sender, foreign_key: :sender_id, class_name: User
-  #belongs_to :recipient, foreign_key: :recipient_id, class_name: User
+  belongs_to :sender, foreign_key: :sender_id, class_name: "User"
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: "User"
  
   validates :sender_id, uniqueness: { scope: :recipient_id }
  

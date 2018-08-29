aa23fde6e847081ef3fdfe373fce6d31a85f6dfe
sbas23 <juan.calderon@aldeamo.com>
2018-08-28 22:03:01 -0500

    Avances chats
    Avances chats

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'login', to: 'login#login' #to: define el controlador y el metodo al que apunta
   resources :index
-  #resources :role
   resources :fundation
   resources :service
   resources :chat
   resources :forum
   resources :calendar
-  #resources :state
+
+  resources :conversations, only: [:create] do
+    member do
+      post :close
+    end
+  end
 end
